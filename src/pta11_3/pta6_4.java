c3645ea9a7ba4f3890428437ab1426a08533f7bb
painye <1850384745@qq.com>
2019-10-16 15:42:07 +0800

    现在要求编写Employee类的toString方法，格式为:父类的toString-company的toString-salary

--- src/pta11_3/pta6_4.java
+++ src/pta11_3/pta6_4.java
@@ -1,12 +1,16 @@
 package pta11_3;
 
 
-
 public class pta6_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("zhang-23-true-IBM-9000.123");
+		
 	}
-
+	
+	public String toString()
+	{
+	    return super.toString()+"-"+company.toString()+"-"+salary;
+	}
+	
 }
