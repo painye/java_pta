85f29ff94379b4b902f193401db20879c4361dc2
painye <1850384745@qq.com>
2019-10-19 11:42:44 +0800

    jmu-Java-03面向对象基础-覆盖与toString

--- src/pta11_3/pta6_4.java
+++ src/pta11_3/pta6_4.java
@@ -5,12 +5,12 @@ public class pta6_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		public String toString()
+		{
+		    return super.toString()+"-"+company.toString()+"-"+salary;
+		}
 	}
 	
-	public String toString()
-	{
-	    return super.toString()+"-"+company.toString()+"-"+salary;
-	}
+	
 	
 }
