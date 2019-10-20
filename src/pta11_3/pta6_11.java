1d08249c7bc182d9567f9825ab8730d0c208c22a
painye <1850384745@qq.com>
2019-10-20 13:57:39 +0800

    sdust-Java-可实现多种排序的Book类

---
+++ src/pta11_3/pta6_11.java
@@ -0,0 +1,21 @@
+package pta11_3;
+
+import java.util.Scanner;
+
+public class pta6_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner in=new Scanner(System.in);
+			int data=in.nextInt();
+			System.out.println(isOdd(data));
+	}
+	
+	//---------------------------------------------------------
+
+	public static boolean isOdd(int data)
+	{
+		if(data%2==0) return true;
+		else	return false;
+	}
+}
